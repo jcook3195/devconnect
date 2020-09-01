30a833bff44f66d04de384deffb8a84ae701366a
jcook3195 <jcook3195@gmail.com>
2020-09-01 17:43:42 -0400

    set up profile/no profile switching logic

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,3 +14,8 @@ export const LOGOUT = 'LOGOUT';
 // user/auth
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+// profile
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
